4aa058de9ced89ea0e296b88ed4da7b85da917dd
Anton <x1x1q1AnToN@gmail.com>
2026-04-15 12:37:11 +0200

    stick

--- src/model/Stick.java
+++ src/model/Stick.java
@@ -4,10 +4,70 @@
  */
 package model;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
 /**
  *
  * @author Anton
  */
 public class Stick {
-	
+
+	private int width = 20;
+	private int height = 50;
+	private Color color;
+	private boolean isVertical;
+	private Point p;
+
+	public Stick(Color color, boolean isVertical, Point p) {
+		this.color = color;
+		this.isVertical = isVertical;
+		this.p = p;
+	}
+
+	public void draw(Graphics g) {
+		g.setColor(color);
+		
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public boolean isIsVertical() {
+		return isVertical;
+	}
+
+	public void setIsVertical(boolean isVertical) {
+		this.isVertical = isVertical;
+	}
+
+	public Point getP() {
+		return p;
+	}
+
+	public void setP(Point p) {
+		this.p = p;
+	}
 }
